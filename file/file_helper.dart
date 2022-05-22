ffc98104409adb994418f831f25e9f91923e1311
AhmedAlaaGenina <32454614+AhmedAlaaGenina@users.noreply.github.com>
2022-05-22 13:41:31 +0200

    Create file_helper.dart

---
+++ file/file_helper.dart
@@ -0,0 +1,15 @@
+class FileHelper {
+  String getFileName(String path) {
+    RegExp regex = RegExp(r'\/([\w-]+\.[\w]{1,})$');
+    Match match = regex.firstMatch(path);
+    if (match != null && match.groupCount == 1) return match.group(1);
+    return '';
+  }
+
+  String getFileExtension(String path) {
+    RegExp regex = RegExp(r'\.([A-Za-z0-9]+)$');
+    Match match = regex.firstMatch(path);
+    if (match != null && match.groupCount == 1) return match.group(1);
+    return '';
+  }
+}
