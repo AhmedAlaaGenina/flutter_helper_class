7d9e3b8f0d2bbe2bda1fe63fb16b03fe24e2dd00
AhmedAlaaGenina <32454614+AhmedAlaaGenina@users.noreply.github.com>
2022-06-06 15:55:45 +0200

    Update connectivity_provider.dart

--- connectivity/connectivity_provider.dart
+++ connectivity/connectivity_provider.dart
@@ -1,53 +1,47 @@
+/// flutter => 3.0.1 
 /// packages
-///  connectivity_plus: ^2.2.0
-///  internet_connection_checker: ^0.0.1+3
+///  connectivity_plus: ^2.3.2
+///  internet_connection_checker: ^0.0.1+4
 ///  open_settings: ^2.0.2
+///  airplane_mode_checker: ^1.0.2
 import 'dart:async';
 
+import 'package:airplane_mode_checker/airplane_mode_checker.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_gen/gen_l10n/app_localization.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:open_settings/open_settings.dart';
 
 class ConnectivityProvider extends ChangeNotifier {
-  ConnectivityResult _connectivityResult = ConnectivityResult.none;
-  InternetConnectionStatus _internetConnectionStatus =
-      InternetConnectionStatus.disconnected;
+  ConnectivityResult? _connectivityType;
+  InternetConnectionStatus? _internetConnectionStatus;
+
+  /// Used to get connection status at any point of time [none],[mobile], [wifi]
+  /// [none] => not connect to [mobile], [wifi] not depended in internet
+  ConnectivityResult? get connectivityType => _connectivityType;
+
+  /// Used to get internet status at any point of time [connected],[disconnected]
+  InternetConnectionStatus? get internetConnectionStatus =>
+      _internetConnectionStatus;
+
+  final Connectivity _connectivity = Connectivity();
+  final InternetConnectionChecker _internet = InternetConnectionChecker();
 
   /// Context for the main screen that holds all screens
   static GlobalKey<NavigatorState> key = GlobalKey<NavigatorState>();
 
+  /// Context for pop Connectivity Dialog
   BuildContext? _connectivityDialogContext;
 
   bool firstTimeOpeningApp = true;
-  final Connectivity _connectivity = Connectivity();
-  final InternetConnectionChecker _internetConnectionChecker =
-      InternetConnectionChecker();
+  bool isDialogOpen = false;
 
-  /// Used to get connection status at any point of time [none],[mobile], [wifi]
-  ConnectivityResult get connectivityResult => _connectivityResult;
-
-  /// Used to get internet status at any point of time [connected],[disconnected]
-  InternetConnectionStatus get internetConnectionStatus =>
-      _internetConnectionStatus;
-
-  /// start point of class and call with provider
   void initialize() async {
-    _internetConnectionChecker.checkInterval = const Duration(seconds: 2);
-    await initConnectivity();
-    if (_internetConnectionStatus == InternetConnectionStatus.disconnected)
-      await listenToConnectionStatus();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initConnectivity() async {
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
-      _internetConnectionStatus = await _internetConnectionChecker.hasConnection
-          ? InternetConnectionStatus.connected
-          : InternetConnectionStatus.disconnected;
+      _internet.onStatusChange.listen(_updateInternetStatus);
+      _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
     } on PlatformException catch (e) {
       debugPrint(
           'Platform does not support queries about status :${e.message}');
@@ -55,30 +49,14 @@ class ConnectivityProvider extends ChangeNotifier {
     }
   }
 
-  Future<void> listenToConnectionStatus() async {
-    /// open stream to listen on change connectivity Status [connected],[disconnected]
-    _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
-
-    /// open stream to listen on change in Internet Connection Status [none],[mobile], [wifi]
-    _internetConnectionChecker.onStatusChange.listen(_updateInternetStatus);
-  }
-
-  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
-    _connectivityResult = result;
-    if (result == ConnectivityResult.none) {
-      showConnectivityDialog();
-    } else {
-      if (_connectivityDialogContext != null) {
-        Navigator.of(_connectivityDialogContext!).pop();
-      }
-    }
-    notifyListeners();
-  }
-
   Future<void> _updateInternetStatus(InternetConnectionStatus status) async {
     _internetConnectionStatus = status;
     print(status);
-    if (status == InternetConnectionStatus.connected) {
+    if ((await AirplaneModeChecker.checkAirplaneMode() ==
+            AirplaneModeStatus.on) &&
+        !isDialogOpen) {
+      showAirPlaneDialog();
+    } else if (status == InternetConnectionStatus.connected) {
       if (firstTimeOpeningApp) {
         firstTimeOpeningApp = false;
       } else {
@@ -87,36 +65,49 @@ class ConnectivityProvider extends ChangeNotifier {
     } else {
       showSnackBar(online: false);
     }
+    notifyListeners();
+  }
 
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    _connectivityType = result;
+    print(result);
+    if ((await AirplaneModeChecker.checkAirplaneMode() ==
+            AirplaneModeStatus.on) &&
+        !isDialogOpen &&
+        result == ConnectivityResult.none) {
+      showAirPlaneDialog();
+    } else if (result == ConnectivityResult.none && !isDialogOpen) {
+      showConnectivityDialog();
+    } else {
+      if (_connectivityDialogContext != null && isDialogOpen) {
+        Navigator.of(_connectivityDialogContext!).pop();
+        isDialogOpen = false;
+      }
+    }
     notifyListeners();
   }
 
   void showSnackBar({required bool online}) {
     if (key.currentContext == null) return;
     ScaffoldMessenger.of(key.currentContext!).hideCurrentSnackBar();
-    ScaffoldMessenger.of(key.currentContext!).showSnackBar(
-      SnackBar(
-        content: Text(
-          online
-              ? AppLocalizations.of(key.currentContext!)!.backOnline
-              : AppLocalizations.of(key.currentContext!)!.youAraOffline,
-          textAlign: TextAlign.center,
-        ),
-        behavior: SnackBarBehavior.floating,
-        backgroundColor: online ? Colors.green : Colors.redAccent,
-        duration:
-            (!online) ? const Duration(days: 365) : const Duration(seconds: 4),
-        action: (!online)
-            ? SnackBarAction(
-                label: AppLocalizations.of(key.currentContext!)!.tryAgain,
-                onPressed: () async {
-                  _updateInternetStatus(
-                      await _internetConnectionChecker.connectionStatus);
-                },
-              )
-            : null,
+    ScaffoldMessenger.of(key.currentContext!).showSnackBar(SnackBar(
+      content: Text(
+        online ? "Back online" : "You ara offline",
+        textAlign: TextAlign.center,
       ),
-    );
+      behavior: SnackBarBehavior.floating,
+      backgroundColor: online ? Colors.green : Colors.redAccent,
+      duration:
+          (!online) ? const Duration(days: 365) : const Duration(seconds: 4),
+      action: (!online)
+          ? SnackBarAction(
+              label: "Try again",
+              onPressed: () async {
+                _updateInternetStatus(await _internet.connectionStatus);
+              },
+            )
+          : null,
+    ));
   }
 
   void showConnectivityDialog() {
@@ -127,23 +118,17 @@ class ConnectivityProvider extends ChangeNotifier {
         builder: (context) {
           _connectivityDialogContext = context;
           return AlertDialog(
-            title: Text(
-                AppLocalizations.of(key.currentContext!)!.connectionDisabled,
-                style: Theme.of(key.currentContext!).textTheme.bodyText1),
-            content: Text(
-                AppLocalizations.of(key.currentContext!)!
-                    .pleaseEnableYourWifiOrMobileData,
-                style: Theme.of(key.currentContext!).textTheme.bodyText1),
+            title: const Text("Connection Disabled"),
+            content: const Text("Please enable your Wifi or Mobile Data"),
             actions: [
               TextButton(
-                child: Text(AppLocalizations.of(key.currentContext!)!.wifi),
+                child: const Text("Wifi"),
                 onPressed: () {
                   OpenSettings.openWIFISetting();
                 },
               ),
               TextButton(
-                child:
-                    Text(AppLocalizations.of(key.currentContext!)!.mobileData),
+                child: const Text("Mobile Data"),
                 onPressed: () {
                   OpenSettings.openDataUsageSetting();
                 },
@@ -151,5 +136,29 @@ class ConnectivityProvider extends ChangeNotifier {
             ],
           );
         });
+    isDialogOpen = true;
+  }
+
+  void showAirPlaneDialog() {
+    if (key.currentContext == null) return;
+    showDialog(
+        context: key.currentContext!,
+        barrierDismissible: false,
+        builder: (context) {
+          _connectivityDialogContext = context;
+          return AlertDialog(
+            title: const Text("Connection Disabled"),
+            content: const Text("Please Disable AirPlane"),
+            actions: [
+              TextButton(
+                child: const Text("Disable Airplane"),
+                onPressed: () {
+                  OpenSettings.openAirplaneModeSetting();
+                },
+              ),
+            ],
+          );
+        });
+    isDialogOpen = true;
   }
 }
