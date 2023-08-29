3b12e50bed59aaa88f207bf06af1acbaa27f959f
ahmedalaa <ahmedalaagenina@gmail.com>
2023-08-29 11:21:09 +0300

    [EDIT]

--- notification_manager/local_notification_api.dart
+++ notification_manager/local_notification_api.dart
@@ -3,14 +3,26 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-import '../main.dart';
-import '../screens/notification_screen.dart';
-
 /// Local Notification Api
 class LocalNotificationApi {
   /// init local notification
   static final FlutterLocalNotificationsPlugin localNotifications =
       FlutterLocalNotificationsPlugin();
+  static const android = AndroidInitializationSettings('app_icon');
+
+  static const DarwinInitializationSettings ios = DarwinInitializationSettings(
+    requestSoundPermission: true,
+    requestBadgePermission: true,
+    requestAlertPermission: true,
+  );
+  static const DarwinInitializationSettings macOS =
+      DarwinInitializationSettings(
+    requestAlertPermission: false,
+    requestBadgePermission: false,
+    requestSoundPermission: false,
+  );
+  static const LinuxInitializationSettings linux =
+      LinuxInitializationSettings(defaultActionName: 'Open notification');
 
   /// init channel
   static const AndroidNotificationChannel androidNotificationChannel =
@@ -24,23 +36,36 @@ class LocalNotificationApi {
 
   /// when tap in notification to open specific screen or anything
   static Future init() async {
-    const android = AndroidInitializationSettings('@mipmap/ic_launcher');
-    const ios = IOSInitializationSettings();
-    const settings = InitializationSettings(android: android, iOS: ios);
+    const initSettings = InitializationSettings(
+      android: android,
+      iOS: ios,
+      macOS: macOS,
+      linux: linux,
+    );
+
+    final bool? resultIos = await localNotifications
+        .resolvePlatformSpecificImplementation<
+            IOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(
+          alert: true,
+          badge: true,
+          sound: true,
+        );
+    final bool? resultMac = await localNotifications
+        .resolvePlatformSpecificImplementation<
+            MacOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(
+          alert: true,
+          badge: true,
+          sound: true,
+        );
     await localNotifications.initialize(
-      settings,
-      onSelectNotification: (payload) async {
-        /// deal with UI
-
-        rootScaffoldMessengerKey.currentState?.showSnackBar(const SnackBar(
-            content:
-                Text("flutter_local_notifications == foregroundNotification")));
-
-        Navigator.of(rootNavigatorKey.currentContext!).push(MaterialPageRoute(
-          builder: (context) =>
-              NotificationScreen(text: payload ?? "foreground"),
-        ));
-      },
+      initSettings,
+      // onDidReceiveBackgroundNotificationResponse: (details) {
+      //   print("onDidReceiveBackgroundNotificationResponse: ${details.payload}");
+      // },
+      /// on did receive notification response = for when app is opened via notification while in foreground on android
+      onDidReceiveNotificationResponse: onTapNotification,
     );
   }
 
@@ -70,9 +95,40 @@ class LocalNotificationApi {
         //channel description
         importance: Importance.max,
         playSound: true,
-        icon: '@mipmap/ic_launcher',
+        icon: 'app_icon',
+        priority: Priority.high,
       ),
-      iOS: const IOSNotificationDetails(),
+      iOS: const DarwinNotificationDetails(),
     );
+  }
+
+  static Map convertPayloadToMap(String payload) {
+    final String nPayload = payload.substring(1, payload.length - 1);
+    List<String> split = [];
+    nPayload.split(",").forEach((String s) => split.addAll(s.split(":")));
+    Map mapped = {};
+    for (int i = 0; i < split.length + 1; i++) {
+      if (i % 2 == 1) {
+        mapped.addAll({split[i - 1].trim().toString(): split[i].trim()});
+      }
+    }
+    return mapped;
+  }
+
+  static void onTapNotification(NotificationResponse details) {
+    debugPrint("onDidReceiveNotificationResponse: ${details.payload}");
+    var map = convertPayloadToMap(details.payload!);
+
+    ///key.currentContext!
+    // if (navigatorKey.currentContext != null) {
+    //   Navigator.of(navigatorKey.currentContext!).push(
+    //     MaterialPageRoute(
+    //       builder: (context) => RejectedDetails(
+    //         transactionId: map['idTransaction'],
+    //         courseId: map['courseId'],
+    //       ),
+    //     ),
+    //   );
+    // }
   }
 }
--- notification_manager/notification_api.dart
+++ notification_manager/notification_api.dart
@@ -1,23 +1,21 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-import '../main.dart';
-import '../screens/notification_screen.dart';
 import 'local_notification_api.dart';
 
 class NotificationApi {
   static FirebaseMessaging messaging = FirebaseMessaging.instance;
 
+  static Future<String?> getDeviceToken() async => await messaging.getToken();
+
   /// How to use it?
   /// in main()
   // await NotificationApi.init();
 
-
   static Future<void> init() async {
-
     // Enabling background Message
     FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
-
     // Enabling foreground notifications for Android
     // by Creating A channel on Android devices
     await LocalNotificationApi.localNotifications
@@ -27,18 +25,24 @@ class NotificationApi {
             LocalNotificationApi.androidNotificationChannel);
 
     // Enabling foreground notifications for IOS
-    await firebaseMessaging.setForegroundNotificationPresentationOptions(
+    await messaging.setForegroundNotificationPresentationOptions(
       alert: true,
       badge: true,
       sound: true,
     );
 
-     LocalNotificationApi.init();
-     NotificationApi.requestPermission();
-     NotificationApi.foregroundNotification();
-     NotificationApi.setupInteractedMessage();
-    
+    LocalNotificationApi.init();
+    NotificationApi.requestPermission();
+    NotificationApi.foregroundNotification();
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      Future.delayed(const Duration(milliseconds: 500), () async {
+        await NotificationApi.setupInteractedMessage();
+      });
+    });
+
+    debugPrint("token: ${getDeviceToken()}");
   }
+
   static void requestPermission() async {
     NotificationSettings settings = await messaging.requestPermission(
       alert: true,
@@ -50,20 +54,21 @@ class NotificationApi {
       sound: true,
     );
 
-    print('User granted permission: ${settings.authorizationStatus}');
+    debugPrint('User granted permission: ${settings.authorizationStatus}');
   }
 
   /// Handle background message only not notification
+  @pragma('vm:entry-point')
   static Future<void> firebaseMessagingBackgroundHandler(
       RemoteMessage message) async {
-    print("Handling a background message: ${message.messageId}");
+    debugPrint("Handling a background message: ${message.data}");
   }
 
-  /// Handle Foreground message and notification
+  /// Handle foreground message and notification
   static void foregroundNotification() {
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('Got a message whilst in the foreground!');
-      print('Message data: ${message.data}');
+      debugPrint('Got a message whilst in the foreground!');
+      debugPrint('Message data: ${message.data}');
 
       RemoteNotification? notification = message.notification;
       AndroidNotification? android = message.notification?.android;
@@ -71,7 +76,8 @@ class NotificationApi {
         /// here handle ui of notification
         /// to get notification must use flutter_local_notifications
 
-        print('Message also contained a notification: ${message.notification}');
+        debugPrint(
+            'Message also contained a notification: ${message.notification}');
 
         /// Interacted with UI in [(Foreground)] (when app is opened)
         /// in LocalNotificationApi.showNotification
@@ -79,8 +85,8 @@ class NotificationApi {
           id: notification.hashCode,
           title: notification.title,
           body: notification.body,
-          payload:
-              "notification.payload", // value that pass from notification to app
+          payload: message.data
+              .toString(), // value that pass from notification to app
         );
       }
     });
@@ -106,17 +112,22 @@ class NotificationApi {
     // navigate to a chat screen
 
     // if app in Background or Terminated that will work fine
-
     /// if (message.data['type'] == 'chat') {
-    print("Opened Notification");
-    print(message.data);
-    rootScaffoldMessengerKey.currentState
-        ?.showSnackBar(const SnackBar(content: Text("Opened Notification")));
+    debugPrint("Opened Notification Data : ${message.data}");
+    // rootScaffoldMessengerKey.currentState
+    //     ?.showSnackBar(const SnackBar(content: Text("Opened Notification")));
 
     ///key.currentContext!
-    Navigator.of(rootNavigatorKey.currentContext!).push(MaterialPageRoute(
-      builder: (context) => const NotificationScreen(),
-    ));
+    // if (navigatorKey.currentContext != null) {
+    //   Navigator.of(navigatorKey.currentContext!).push(
+    //     MaterialPageRoute(
+    //       builder: (context) => RejectedDetails(
+    //         transactionId: message.data['idTransaction'],
+    //         courseId: message.data['courseId'],
+    //       ),
+    //     ),
+    //   );
+    // }
 
     /// }
   }
