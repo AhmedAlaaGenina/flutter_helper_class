f514d07c8715ac118da6b85276cd8b12e0682e1e
ahmedalaa <ahmedalaagenina@gmail.com>
2024-02-20 10:13:14 +0200

    [ADD] google map helper

---
+++ google_map/google_map_helper.dart
@@ -0,0 +1,21 @@
+// flutter_polyline_points: ^1.0.0
+
+class GoogleMapHelper {
+  Future<List<LatLng>> getPolyPoints(
+      LatLng sourceLocation, LatLng destination) async {
+    List<LatLng> polylineCoordinates = [];
+    PolylinePoints polylinePoints = PolylinePoints();
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      google_api_key, // Your Google Map Key
+      PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
+      PointLatLng(destination.latitude, destination.longitude),
+    );
+    if (result.points.isNotEmpty) {
+      result.points.forEach(
+        (PointLatLng point) =>
+            polylineCoordinates.add(LatLng(point.latitude, point.longitude)),
+      );
+    }
+    return polylineCoordinates;
+  }
+}
