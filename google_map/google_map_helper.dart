618b2c5556ceb55e8b9e08d2321267327c815dc9
ahmedalaa <ahmedalaagenina@gmail.com>
2024-02-21 11:30:41 +0200

    [EDIT] location and map

--- google_map/google_map_helper.dart
+++ google_map/google_map_helper.dart
@@ -1,37 +1,109 @@
-// flutter_polyline_points: ^1.0.0
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class GoogleMapHelper {
-  Future<Map<PolylineId, Polyline>> getPolyLine(
-      LatLng sourceLocation, LatLng destination) async {
-    // how to use it
-    // in google map widget
-    // GoogleMap(
-    // ...
-    // polylines: Set<Polyline>.of(polylines.values),
-    // ),
+  GoogleMapHelper._();
 
-   static List<LatLng> polylineCoordinates = [];
+  static const String _googleApiKey =
+      'your_google_api_key_here'; // Replace with your Google API Key
+
+  //? how to use it
+  // in google map widget
+  // GoogleMap(
+  // ...
+  // polylines: Set<Polyline>.of(polylines.values),
+  // ),
+  /// Retrieves polyline between source and destination locations.
+  static Future<Map<PolylineId, Polyline>> getPolyline(
+    LatLng sourceLocation,
+    LatLng destination,
+    Color polylineColor,
+  ) async {
+    List<LatLng> polylineCoordinates = [];
     PolylinePoints polylinePoints = PolylinePoints();
     PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
-      google_api_key, // Your Google Map Key
+      _googleApiKey,
       PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
       PointLatLng(destination.latitude, destination.longitude),
     );
     if (result.points.isNotEmpty) {
-      result.points.forEach(
-        (PointLatLng point) =>
-            polylineCoordinates.add(LatLng(point.latitude, point.longitude)),
-      );
+      polylineCoordinates = result.points
+          .map((point) => LatLng(point.latitude, point.longitude))
+          .toList();
     }
-   return _addPolyLine(polylineCoordinates);
+    return _addPolyLine(polylineCoordinates, polylineColor);
   }
 
- static Map<PolylineId, Polyline> _addPolyLine(List<LatLng> polylineCoordinates) {
-     Map<PolylineId, Polyline> polylines = {};
-    PolylineId id = PolylineId("poly");
-    Polyline polyline = Polyline(
-        polylineId: id, color: Colors.red, points: polylineCoordinates,);
-    polylines[id] = polyline;
-    return polylines;
+  /// Generates camera position for displaying location image preview.
+  static CameraPosition generateLocationImagePreview({
+    required double latitude,
+    required double longitude,
+  }) {
+    return CameraPosition(
+      target: LatLng(latitude, longitude),
+      zoom: 16,
+    );
+  }
+
+  /// Generates marker for displaying location marker preview.
+  static Future<Marker> generateLocationMarkerPreview({
+    required double latitude,
+    required double longitude,
+    required String markerAssetPath,
+    required int markerWidth,
+  }) async {
+    final Uint8List markerIcon = await getBytesFromAsset(
+      path: markerAssetPath,
+      width: markerWidth,
+    );
+    return Marker(
+      markerId: const MarkerId('Marker'),
+      position: LatLng(latitude, longitude),
+      icon: BitmapDescriptor.fromBytes(markerIcon),
+    );
+  }
+
+  // set custom marker with width
+  static Future<Uint8List> getBytesFromAsset({
+    required String path,
+    required int width,
+  }) async {
+    final ByteData data = await rootBundle.load(path);
+    final ui.Codec codec = await ui.instantiateImageCodec(
+      data.buffer.asUint8List(),
+      targetWidth: width,
+    );
+    final ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!
+        .buffer
+        .asUint8List();
+  }
+
+  // set custom marker without width
+  BitmapDescriptor setCustomMarkerIcon(String path) {
+    BitmapDescriptor sourceIcon = BitmapDescriptor.defaultMarker;
+
+    BitmapDescriptor.fromAssetImage(ImageConfiguration.empty, path)
+        .then((icon) => sourceIcon = icon);
+
+    return sourceIcon;
+  }
+
+  static Map<PolylineId, Polyline> _addPolyLine(
+    List<LatLng> polylineCoordinates,
+    Color polylineColor,
+  ) {
+    const PolylineId id = PolylineId("poly");
+    final Polyline polyline = Polyline(
+      polylineId: id,
+      color: polylineColor,
+      points: polylineCoordinates,
+    );
+    return {id: polyline};
   }
 }
--- location/location_helper.dart
+++ location/location_helper.dart
@@ -1,102 +1,72 @@
-import 'dart:typed_data';
-import 'dart:ui' as ui;
-
-import 'package:fashion/generated/assets.gen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:geocoding/geocoding.dart' as geo;
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
 // const GOOGLE_API_KEY = 'AIzaSyCvrYkbhs9PAMtEAaTwnXrFtQaBz-GS60Q';
 const GOOGLE_API_KEY = 'AIzaSyBT30Wp2jPm5sw9DrP9qTge8SZgBsAO_wI';
 
 class LocationHelper {
-  static Future<LocationData?> getCurrentLocation() async {
-    Location location = Location();
-    bool serviceEnabled;
-    PermissionStatus permissionGranted;
+  LocationHelper._();
 
-    serviceEnabled = await location.serviceEnabled();
+  static Location location = Location();
+
+  static const double _distanceFilter = 1; // in meters
+  static const int _updateInterval = 10; // in seconds
+
+  /// Checks and requests location permissions if needed.
+  static Future<bool> checkLocationPermission() async {
+    bool serviceEnabled = await location.serviceEnabled();
     if (!serviceEnabled) {
       serviceEnabled = await location.requestService();
       if (!serviceEnabled) {
-        return null;
+        return false;
       }
     }
-
-    permissionGranted = await location.hasPermission();
+    PermissionStatus permissionGranted = await location.hasPermission();
     if (permissionGranted == PermissionStatus.denied) {
       permissionGranted = await location.requestPermission();
       if (permissionGranted != PermissionStatus.granted) {
-        return null;
+        return false;
       }
     }
+    return true;
+  }
+
+  /// Retrieves the current device location.
+  static Future<LocationData?> getCurrentLocation() async {
+    bool havePermission = await checkLocationPermission();
+    if (!havePermission) return null;
     return await location.getLocation();
   }
 
-  static CameraPosition generateLocationImagePreview({
-    required double latitude,
-    required double longitude,
-  }) {
-    return CameraPosition(
-      target: LatLng(latitude, longitude),
-      zoom: 16,
-    );
+  /// Starts listening for location updates.
+  static Future<void> startListeningLocationChanged(
+      ValueChanged<LocationData> onChangeLocation) async {
+    bool havePermission = await checkLocationPermission();
+    if (!havePermission) return;
+    location.enableBackgroundMode(enable: true);
+    location.changeSettings(
+        interval: _updateInterval, distanceFilter: _distanceFilter);
+    location.onLocationChanged.listen(onChangeLocation);
   }
 
-  static Future<Marker> generateLocationMarkerPreview({
-    required double latitude,
-    required double longitude,
-  }) async {
-    final Uint8List customMarker = await getBytesFromAsset(
-      path: Assets.images.marker.path,
-      width: 72,
-    );
-    return Marker(
-      markerId: const MarkerId('Marker'),
-      position: LatLng(latitude, longitude),
-      icon: BitmapDescriptor.fromBytes(customMarker),
-    );
+  /// Stops listening for location updates.
+  static Future<void> stopListeningLocation() async {
+    await location.enableBackgroundMode(enable: false);
   }
 
-  // set custom marker with width
-  static Future<Uint8List> getBytesFromAsset({
-    required String path,
-    required int width,
-  }) async {
-    ByteData data = await rootBundle.load(path);
-    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
-        targetWidth: width);
-    ui.FrameInfo fi = await codec.getNextFrame();
-    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!
-        .buffer
-        .asUint8List();
-  }
-
-  // set custom marker without width
-  BitmapDescriptor setCustomMarkerIcon(String path) {
-    BitmapDescriptor sourceIcon = BitmapDescriptor.defaultMarker;
-
-    BitmapDescriptor.fromAssetImage(ImageConfiguration.empty, path)
-        .then((icon) => sourceIcon = icon);
-
-    return sourceIcon;
-  }
-
+  /// Retrieves address information from coordinates.
   static Future<List<geo.Placemark>> getPlaceAddress({
     required double latitude,
     required double longitude,
   }) async {
-    List<geo.Placemark> placeMarks =
-        await geo.placemarkFromCoordinates(latitude, longitude);
-    return placeMarks;
+    return await geo.placemarkFromCoordinates(latitude, longitude);
   }
 
+  /// Retrieves coordinates from an address string.
   static Future<List<geo.Location>> getLocationAddress({
     required String address,
   }) async {
-    List<geo.Location> locations = await geo.locationFromAddress(address);
-    return locations;
+    return await geo.locationFromAddress(address);
   }
 }
