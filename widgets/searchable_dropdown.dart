cc62055a61a0f52658db37155a6dade6b816d4a8
AhmedAlaaGenina <ahmed.alaa4080@foobar.com>
2024-11-12 15:01:19 +0200

    Enhance SearchableDropdown with customizable properties and improved dropdown behavior

--- widgets/searchable_dropdown.dart
+++ widgets/searchable_dropdown.dart
@@ -9,6 +9,25 @@ class SearchableDropdown<T> extends StatefulWidget {
   final String Function(T) getLabel;
   final TextEditingController? controller;
   final List<T> Function(String) searchableFunction;
+  final double? width;
+  final EdgeInsetsGeometry contentPadding;
+  final EdgeInsetsGeometry margin;
+  final double dropdownMaxHeight;
+  final double borderRadius;
+  final Color? backgroundColor;
+  final Color? dropdownBackgroundColor;
+  final TextStyle? hintStyle;
+  final TextStyle? textStyle;
+  final TextStyle? dropdownTextStyle;
+  final BoxDecoration? dropdownDecoration;
+  final InputDecoration? inputDecoration;
+  final Widget? suffixIcon;
+  final Widget? prefixIcon;
+  final Widget? notFoundWidget;
+  final double dropdownElevation;
+  final double dropdownOffset;
+  final InputBorder? border;
+
   const SearchableDropdown({
     super.key,
     required this.hint,
@@ -19,6 +38,24 @@ class SearchableDropdown<T> extends StatefulWidget {
     this.onNotFound,
     this.onSearchResult,
     this.controller,
+    this.width,
+    this.contentPadding = EdgeInsets.zero,
+    this.margin = EdgeInsets.zero,
+    this.dropdownMaxHeight = 200,
+    this.borderRadius = 8,
+    this.backgroundColor,
+    this.dropdownBackgroundColor,
+    this.hintStyle,
+    this.textStyle,
+    this.dropdownTextStyle,
+    this.dropdownDecoration,
+    this.inputDecoration,
+    this.suffixIcon,
+    this.prefixIcon,
+    this.notFoundWidget,
+    this.dropdownElevation = 4,
+    this.dropdownOffset = 5,
+    this.border,
   });
 
   @override
@@ -42,7 +79,11 @@ class _SearchableDropdownState<T> extends State<SearchableDropdown<T>> {
       if (_focusNode.hasFocus) {
         _openDropdown();
       } else {
-        _closeDropdown();
+        // Delay closing dropdown to allow item selection on tap
+        Future.delayed(
+          const Duration(milliseconds: 200),
+          () => _closeDropdown(),
+        );
       }
     });
   }
@@ -82,49 +123,62 @@ class _SearchableDropdownState<T> extends State<SearchableDropdown<T>> {
 
     return OverlayEntry(
       builder: (context) => Positioned(
-        width: size.width,
+        width: widget.width ?? size.width,
         child: CompositedTransformFollower(
           link: _layerLink,
           showWhenUnlinked: false,
-          offset: Offset(0, size.height + 5),
+          offset: Offset(0, size.height + widget.dropdownOffset),
           child: Material(
-            elevation: 4,
-            borderRadius: BorderRadius.circular(8),
+            elevation: widget.dropdownElevation,
+            borderRadius: BorderRadius.circular(widget.borderRadius),
+            color: Colors.transparent,
             child: Container(
               constraints: BoxConstraints(
-                maxHeight: 200,
-                maxWidth: size.width,
+                maxHeight: widget.dropdownMaxHeight,
+                maxWidth: widget.width ?? size.width,
               ),
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.surface,
-                borderRadius: BorderRadius.circular(8),
-                border: Border.all(
-                  color: Theme.of(context).colorScheme.outline,
-                  width: 1,
-                ),
-              ),
+              decoration: widget.dropdownDecoration ??
+                  BoxDecoration(
+                    color: widget.dropdownBackgroundColor ??
+                        Theme.of(context).colorScheme.surface,
+                    borderRadius: BorderRadius.circular(widget.borderRadius),
+                    border: Border.all(
+                      color: Theme.of(context).colorScheme.outline,
+                      width: 1,
+                    ),
+                  ),
               child: SingleChildScrollView(
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
                     if (_filteredItems.isEmpty) ...[
-                      ListTile(
-                        title: const Text('No items found'),
-                        trailing: widget.onNotFound != null
-                            ? TextButton(
-                                onPressed: () {
-                                  _closeDropdown();
-                                  widget.onNotFound!();
-                                },
-                                child: const Text('Add New'),
-                              )
-                            : null,
-                      ),
+                      widget.notFoundWidget ??
+                          ListTile(
+                            title: Text(
+                              'No items found',
+                              style: widget.dropdownTextStyle,
+                            ),
+                            trailing: widget.onNotFound != null
+                                ? TextButton(
+                                    onPressed: () {
+                                      _closeDropdown();
+                                      widget.onNotFound!();
+                                    },
+                                    child: const Text('Add New'),
+                                  )
+                                : null,
+                          ),
                     ] else
                       ..._filteredItems.map(
                         (item) => ListTile(
-                          title: Text(widget.getLabel(item)),
-                          onTap: () => _selectItem(item),
+                          contentPadding: widget.contentPadding,
+                          title: Text(
+                            widget.getLabel(item),
+                            style: widget.dropdownTextStyle,
+                          ),
+                          onTap: () {
+                            _selectItem(item);
+                          },
                         ),
                       ),
                   ],
@@ -138,11 +192,9 @@ class _SearchableDropdownState<T> extends State<SearchableDropdown<T>> {
   }
 
   void _onSearchChanged(String value) {
-    setState(
-      () {
-        _filteredItems = widget.searchableFunction(value);
-      },
-    );
+    setState(() {
+      _filteredItems = widget.searchableFunction(value);
+    });
     if (widget.onSearchResult != null) widget.onSearchResult!(_filteredItems);
 
     if (_isOpen) {
@@ -152,25 +204,35 @@ class _SearchableDropdownState<T> extends State<SearchableDropdown<T>> {
 
   @override
   Widget build(BuildContext context) {
-    return CompositedTransformTarget(
-      link: _layerLink,
-      child: TextFormField(
-        controller: _controller,
-        focusNode: _focusNode,
-        onChanged: _onSearchChanged,
-        decoration: InputDecoration(
-          hintText: widget.hint,
-          border: const OutlineInputBorder(),
-          suffixIcon: IconButton(
-            icon: const Icon(Icons.arrow_drop_down),
-            onPressed: () {
-              if (_isOpen) {
-                _focusNode.unfocus();
-              } else {
-                _focusNode.requestFocus();
-              }
-            },
-          ),
+    return Container(
+      width: widget.width,
+      margin: widget.margin,
+      child: CompositedTransformTarget(
+        link: _layerLink,
+        child: TextFormField(
+          controller: _controller,
+          focusNode: _focusNode,
+          onChanged: _onSearchChanged,
+          style: widget.textStyle,
+          decoration: widget.inputDecoration ??
+              InputDecoration(
+                hintText: widget.hint,
+                hintStyle: widget.hintStyle,
+                border: widget.border ?? const OutlineInputBorder(),
+                contentPadding: widget.contentPadding,
+                prefixIcon: widget.prefixIcon,
+                suffixIcon: widget.suffixIcon ??
+                    IconButton(
+                      icon: const Icon(Icons.arrow_drop_down),
+                      onPressed: () {
+                        if (_isOpen) {
+                          _focusNode.unfocus();
+                        } else {
+                          _focusNode.requestFocus();
+                        }
+                      },
+                    ),
+              ),
         ),
       ),
     );
